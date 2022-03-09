3f7dc38acee18de99e9fd7685e122a04098a192c
latorware <69585111+latorware@users.noreply.github.com>
2022-03-09 17:44:32 +0100

    l

---
+++ macos/FreeGLUT/OpenGL/04-transforms/build/CMakeFiles/transforms.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/davidlatorre/Documents/GitHub/CELESTE_PICO-8/macos/FreeGLUT/OpenGL/04-transforms/Game.cpp" "CMakeFiles/transforms.dir/Game.cpp.o" "gcc" "CMakeFiles/transforms.dir/Game.cpp.o.d"
+  "/Users/davidlatorre/Documents/GitHub/CELESTE_PICO-8/macos/FreeGLUT/OpenGL/04-transforms/Quad.cpp" "CMakeFiles/transforms.dir/Quad.cpp.o" "gcc" "CMakeFiles/transforms.dir/Quad.cpp.o.d"
+  "/Users/davidlatorre/Documents/GitHub/CELESTE_PICO-8/macos/FreeGLUT/OpenGL/04-transforms/Scene.cpp" "CMakeFiles/transforms.dir/Scene.cpp.o" "gcc" "CMakeFiles/transforms.dir/Scene.cpp.o.d"
+  "/Users/davidlatorre/Documents/GitHub/CELESTE_PICO-8/macos/FreeGLUT/OpenGL/04-transforms/Shader.cpp" "CMakeFiles/transforms.dir/Shader.cpp.o" "gcc" "CMakeFiles/transforms.dir/Shader.cpp.o.d"
+  "/Users/davidlatorre/Documents/GitHub/CELESTE_PICO-8/macos/FreeGLUT/OpenGL/04-transforms/ShaderProgram.cpp" "CMakeFiles/transforms.dir/ShaderProgram.cpp.o" "gcc" "CMakeFiles/transforms.dir/ShaderProgram.cpp.o.d"
+  "/Users/davidlatorre/Documents/GitHub/CELESTE_PICO-8/macos/FreeGLUT/OpenGL/04-transforms/main.cpp" "CMakeFiles/transforms.dir/main.cpp.o" "gcc" "CMakeFiles/transforms.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ macos/FreeGLUT/OpenGL/04-transforms/build/CMakeFiles/transforms.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/transforms.dir/Game.cpp.o"
+  "CMakeFiles/transforms.dir/Game.cpp.o.d"
+  "CMakeFiles/transforms.dir/Quad.cpp.o"
+  "CMakeFiles/transforms.dir/Quad.cpp.o.d"
+  "CMakeFiles/transforms.dir/Scene.cpp.o"
+  "CMakeFiles/transforms.dir/Scene.cpp.o.d"
+  "CMakeFiles/transforms.dir/Shader.cpp.o"
+  "CMakeFiles/transforms.dir/Shader.cpp.o.d"
+  "CMakeFiles/transforms.dir/ShaderProgram.cpp.o"
+  "CMakeFiles/transforms.dir/ShaderProgram.cpp.o.d"
+  "CMakeFiles/transforms.dir/main.cpp.o"
+  "CMakeFiles/transforms.dir/main.cpp.o.d"
+  "transforms"
+  "transforms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/transforms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
