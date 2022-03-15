2f3b70306e6c0274365b57637831a9d519384cae
david latorre <davidlatorremotos@gmail.com>
2022-03-15 10:54:47 +0100

    a

---
+++ openGL/2DGame/02-Bubble/02-Bubble/MainMenu.h
@@ -0,0 +1,21 @@
+#ifndef _MAINMENU_INCLUDE
+#define _MAINMENU_INCLUDE
+
+
+#include "Scene.h"
+
+
+
+class MainMenu : public Scene {
+public:
+	MainMenu(); 
+	~MainMenu();
+
+	void init(); 
+
+private: 
+
+};
+
+
+#endif //
