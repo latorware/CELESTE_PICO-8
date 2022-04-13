7ce744e9be90f14ff423d163ea0688c08299d333
david latorre <davidlatorremotos@gmail.com>
2022-04-13 18:01:42 +0200

    reordenat i eliminat coses

---
+++ other/tileset 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset 1" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="repaired sheet.png" width="128" height="128"/>
+</tileset>
